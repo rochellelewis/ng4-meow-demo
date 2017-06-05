dd81b3f67b3411841bfc2ac53312948c6f019837
Rochelle Lewis <rlewis37@gmail.com>
2017-06-05 16:35:44 -0600

    add static UI files

---
+++ static-ui/about.php
@@ -0,0 +1,30 @@
+<?php require_once("lib/head-utils.php");?>
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- insert header and navbar -->
+		<?php require_once("lib/header.php");?>
+
+		<main class="bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<h1>About Meow</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<p>Meow for food, then when human fills food dish, take a few bites of food and continue meowing fall asleep on the washing machine hunt by meowing loudly at 5am next to human slave food dispenser. Rub face on owner lies down but you call this cat food?. Kick up litter put butt in owner's face and wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again and eats owners hair then claws head.</p>
+					</div>
+					<div class="col-md-6">
+						<p>When in doubt, wash eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap hunt anything that moves, and purr while eating pee in human's bed until he cleans the litter box inspect anything brought into the house purr.</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div><!--/.sfooter-content-->
+
+	<!-- insert footer -->
+	<?php require_once("lib/footer.php");?>
+</body>
+</html>
