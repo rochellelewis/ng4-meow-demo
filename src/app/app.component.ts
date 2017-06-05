dff2df4d3ab5844954425f11e82b1b57b2266efd
Rochelle Lewis <rlewis37@gmail.com>
2017-06-05 17:00:22 -0600

    add base /src files and base templates

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component, ViewChild} from "@angular/core";
+
+@Component({
+	// Update selector with YOUR_APP_NAME-app. This needs to match the custom tag in webpack/index.php
+	selector: 'ng4-meow-demo',
+
+	// templateUrl path to your public_html/templates directory.
+	templateUrl: './templates/ng4-meow-demo.php'
+})
+
+export class AppComponent {}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./components/home.component";
+
+
+export const allAppComponents = [HomeComponent];
+
+export const routes: Routes = [
+	{path: "", component: HomeComponent},
+	{path: "**", redirectTo: ""}
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing = RouterModule.forRoot(routes);
