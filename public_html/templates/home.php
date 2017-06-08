45a0f817d8c1c2dee0465157466efb789e918ab3
Rochelle Lewis <rlewis37@cnm.edu>
2017-06-08 11:29:47 -0600

    added home template

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,1 +1,12 @@
-<h1>Home Template</h1>
+<main class="bg p-t-nav">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-4">
+				<div class="jumbotron text-right">
+					<h1>Welcome to the Meow App!</h1>
+					<p class="lead">Now with 100% more fuzzy! =^. .^=</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
