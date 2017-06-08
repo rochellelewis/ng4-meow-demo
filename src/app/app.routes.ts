55836f7e15d74c4b8571832e88b4f8f1b4334af6
Rochelle Lewis <rlewis37@cnm.edu>
2017-06-08 11:41:08 -0600

    added about template, route, component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
+import {AboutComponent} from "./components/about.component";
 
 
-export const allAppComponents = [HomeComponent];
+export const allAppComponents = [HomeComponent, AboutComponent];
 
 export const routes: Routes = [
+	{path: "about", component: AboutComponent},
 	{path: "", component: HomeComponent},
 	{path: "**", redirectTo: ""}
 ];
