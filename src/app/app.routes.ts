f8da078d371ae059caf58ef2db7463b86d1a49e5
Rochelle Lewis <rlewis37@cnm.edu>
2017-06-08 16:27:27 -0600

    IMPORTANT: forgot to add post service to routing providers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {AboutComponent} from "./components/about.component";
 import {PostsComponent} from "./components/posts.component";
 import {NavbarComponent} from "./components/navbar.component";
 import {FooterComponent} from "./components/footer.component";
+import {PostService} from "./services/post.service";
 
 
 export const allAppComponents = [HomeComponent, AboutComponent, PostsComponent, NavbarComponent, FooterComponent];
@@ -15,6 +16,6 @@ export const routes: Routes = [
 	{path: "**", redirectTo: ""}
 ];
 
-export const appRoutingProviders: any[] = [];
+export const appRoutingProviders: any[] = [PostService];
 
 export const routing = RouterModule.forRoot(routes);
