e4ed56bb6c097b41df42bcaf30ddb6f2dd11925b
Rochelle Lewis <rlewis37@cnm.edu>
2017-06-08 15:58:42 -0600

    added get all posts to use the service

--- src/app/components/posts.component.ts
+++ src/app/components/posts.component.ts
@@ -1,9 +1,21 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Params} from "@angular/router";
 import {Observable} from "rxjs";
+import {PostService} from "../services/post.service";
+import {Post} from "../classes/post";
 
 @Component({
 	templateUrl: "./templates/posts.php"
 })
 
-export class PostsComponent {}
+export class PostsComponent {
+
+	posts : Post[] = [];
+
+	constructor(private postService : PostService) {}
+
+	getAllPosts() : void {
+		this.postService.getAllPosts()
+			.subscribe(posts => this.posts = posts);
+	}
+}
