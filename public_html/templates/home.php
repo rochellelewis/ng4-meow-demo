e351e0367837d2a9e789ef200d70e4d72b3019cc
Rochelle Lewis <rlewis37@cnm.edu>
2017-08-24 10:12:49 -0600

    added home page html to angular template

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,1 +1,60 @@
-<h1>This is the Angular home page template.</h1>
+<div class="sfooter">
+	<div class="sfooter-content">
+
+		<header>
+			<nav class="navbar navbar-inverse">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="">Meow App!</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href=""><i class="fa fa-home"></i></a></li>
+							<li><a href="about.php">About</a></li>
+							<li><a href="posts.php">Posts</a></li>
+							<li><a href="https://github.com/rlewis2892/ng4-meow-demo" target="_blank">GitHub</a></li>
+						</ul>
+
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</header>
+
+		<main class="bg p-t-nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-4">
+						<div class="jumbotron text-right">
+							<h1>Welcome to the Meow App!</h1>
+							<p class="lead">Now with 100% more fuzzy! =^. .^=</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+
+	</div>
+
+	<footer class="navbar-fixed-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					=^. .^=
+				</div>
+				<div class="col-sm-6 text-right-sm">
+					Meow!
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
