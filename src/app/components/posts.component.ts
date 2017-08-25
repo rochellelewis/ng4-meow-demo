f919d8601d300784449229b89e9df4a57f9ba83f
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-25 11:44:19 -0600

    added create post to component

--- src/app/components/posts.component.ts
+++ src/app/components/posts.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Params} from "@angular/router";
 import {Observable} from "rxjs";
 import {PostService} from "../services/post-service";
 import {Post} from "../classes/post";
+import {Status} from "../classes/status";
 
 @Component({
 	templateUrl: "./templates/posts.php"
@@ -10,7 +11,9 @@ import {Post} from "../classes/post";
 
 export class PostsComponent implements OnInit {
 
+	newPost : Post = new Post(null, null, null, null);
 	posts : Post[] = [];
+	status : Status = new Status(null, null, null);
 
 	constructor(protected postService: PostService) {}
 
@@ -21,5 +24,15 @@ export class PostsComponent implements OnInit {
 	getAllPosts() : void {
 		this.postService.getAllPosts()
 			.subscribe(posts => this.posts = posts);
+	}
+
+	createPost() : void {
+		this.postService.createPost(this.newPost)
+			.subscribe(status => {
+				this.status = status;
+				if(this.status.status === 200) {
+					this.getAllPosts();
+				}
+			});
 	}
 }
