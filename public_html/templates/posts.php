712549af8135e6ad391cae927df096154b526475
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-25 11:59:40 -0600

    finalized posting of posts before going postal - :+1: to like this post

--- public_html/templates/posts.php
+++ public_html/templates/posts.php
@@ -10,14 +10,14 @@
 						<h1>Create New Post</h1>
 
 						<!-- Create New Post Form -->
-						<form id="contact-form">
+						<form #postForm="ngForm" id="postForm" (submit)="createPost();">
 							<div class="form-group">
 								<label class="sr-only" for="postTitle">Title <span class="text-danger">*</span></label>
 								<div class="input-group">
 									<div class="input-group-addon">
 										<i class="fa fa-pencil" aria-hidden="true"></i>
 									</div>
-									<input type="text" class="form-control" id="postTitle" name="postTitle" placeholder="Post Title">
+									<input type="text" class="form-control" id="postTitle" name="postTitle" placeholder="Post Title" [(ngModel)]="newPost.postTitle" #postTitle="ngModel" />
 								</div>
 							</div>
 							<div class="form-group">
@@ -26,13 +26,18 @@
 									<div class="input-group-addon">
 										<i class="fa fa-envelope" aria-hidden="true"></i>
 									</div>
-									<textarea class="form-control" name="postContent" id="postContent" cols="30" rows="10" placeholder="1024 characters max."></textarea>
+									<textarea class="form-control" name="postContent" id="postContent" cols="30" rows="10" placeholder="1024 characters max." [(ngModel)]="newPost.postContent" #postContent="ngModel" ></textarea>
 								</div>
 							</div>
 
-							<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Submit</button>
+							<button [disabled]="postForm.invalid" class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Submit</button>
 							<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
 						</form>
+
+						<div *ngIf="status !== null" class="alert alert-dismissible" [ngClass]="status.type" role="alert">
+							<button type="button" class="close" aria-label="Close" (click)="status = null;"><span aria-hidden="true">&times;</span></button>
+							{{ status.message }}
+						</div>
 
 					</div>
 
@@ -47,7 +52,6 @@
 							<div class="panel-body">
 								{{ post.postContent }}
 							</div>
-							<pre>{{ post | json }}</pre>
 						</div>
 					</div>
 				</div>
